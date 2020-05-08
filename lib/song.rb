a50d9bf1e6f2f0575008dd6d25a74d9206f4c702
Pat McClernan <patmccler@gmail.com>
2020-05-08 16:14:42 -0400

    implements slugify

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,14 @@
+require "pry"
 class Song
-  # code goes here
+  attr_accessor :title, :artist
+
+  def slugify
+    artist_with_song = "#{@artist.name} - #{@title}"
+    slug = @title.downcase.tr(" ", "_").to_s
+
+    file = Tempfile.new([slug, ".txt"], "tmp")
+    file.write(artist_with_song)
+    file.close
+    # File.open("./tmp/#{slug}.txt", "w") { |f| f.write artist_with_song }
+  end
 end
