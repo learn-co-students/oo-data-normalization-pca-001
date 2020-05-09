6385c32f843f9e8d6373a3575687a137bd5903f6
Andrew Kloecker <andrew.kloecker@MacBook-Pro.local>
2020-05-09 15:40:45 -0400

    lesson complete

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,10 @@
 class Song
-  # code goes here
+  attr_accessor :title, :artist
+  def slugify
+    file_name = @title.split(' ').collect{ |w| w.downcase }.join("_")
+    file_write = "#{@artist.name} - #{@title}"
+    file = File.open("./tmp/#{file_name}.txt", "w")
+    file.puts(file_write)
+    file.close
+  end
 end
