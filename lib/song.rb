20226d4d3236308c3478b3041b7ecef4e267dd47
Stephen McKeon <stephen.mckeon@powerhrg.com>
2020-05-10 16:14:02 -0400

    Makes temp files

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,10 @@
 class Song
-  # code goes here
+  require "tempfile"
+  attr_accessor :title, :artist
+
+  def slugify
+    file = Tempfile.new([title.downcase.gsub(" ", "_"), ".txt"], "tmp")
+    file.write(@artist.name + " - " + title)
+    file.close
+  end
 end
